21a720a63c12b594d189e352ca08fc89fa529b22
Prayag <prayag@Prayag.(none)>
2012-12-12 18:09:25 +0545

    update : User

--- grails-app/domain/eccount/User.groovy
+++ grails-app/domain/eccount/User.groovy
@@ -4,14 +4,18 @@ package eccount
   * @created : 9 Nov, 2012
   */
 class User{
+    String firstName;
+    String middleName;
+    String lastName;
     String username;
     String password;
     Boolean active=true;
-    
+    Date created = new Date();
     //static mapWith="mongo"
     
     static constraints = {
          password(password:true)
          active(display:false)
+         created(display:false)
     }
 }
