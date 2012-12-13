13283e049b1ddcb8b6e1c8f23aceea80dd18d3c3
iPrayag <prayag.upd@gmail.com>
2012-12-14 00:06:15 +0545

    UPDATE : customer attributes order

--- grails-app/domain/eccount/Customer.groovy
+++ grails-app/domain/eccount/Customer.groovy
@@ -9,10 +9,14 @@ class Customer{
     String middleName;
     String lastName;	
     Boolean active=true;
-
+    Date created = new Date();
     //static mapWith="mongo"
 
     static constraints = {
+         firstName(blank:false);
+         middleName(blank:true);
+         lastName(blank:false);
          active(display:false)
+         created(blank:false);
     }
 }
