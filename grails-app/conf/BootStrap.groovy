760842dd91221ea4f4062caa59dfd1fb73f90d0a
Prayag <prayag@Prayag.(none)>
2012-12-27 18:18:52 +0545

    ADD admin user to app bootStrap

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,7 +1,17 @@
+/**
+  * @author : Prayag Upd
+  * @since  : 27.12.2012 
+  */
+import eccount.User;
+
 class BootStrap {
 
-    def init = { servletContext ->
-    }
-    def destroy = {
-    }
+	def init = { servletContext ->
+		new User(firstName:"Prayag",
+			 lastName:"Upd",
+			 username:"prayag.upd@gmail.com",
+                         password:"123456").save()
+	}
+	def destroy = {
+	}
 }
