bac6b901604e0815464d9c084009155d9e1c575b
Prayag <prayag.upd@gmail.com>
2013-01-25 20:31:41 +0545

    UPDATE domain user after s2-quickstart for spring security plugin

--- grails-app/domain/eccount/User.groovy
+++ grails-app/domain/eccount/User.groovy
@@ -1,28 +1,53 @@
 package eccount
-/**
-  * @author  : Prayag Upd
-  * @created : 9 Nov, 2012
-  */
-class User{
-    String firstName;
+
+class User {
+
+	transient springSecurityService
+	
+	String firstName;
     String middleName;
     String lastName;
-    String username;
-    String password;
-    Boolean active=true;
-    Date created = new Date();
-    //static mapWith="mongo"
     
-    static constraints = {
-         firstName(blank:false)
-         middleName(blank:true)
-         lastName(blank:false)
-	 username(unique:true)
-         password(password:true)
-         active(display:false)
-         created(display:false)
-    }
-    String toString(){
+	String username
+	String password
+	boolean enabled
+	boolean accountExpired
+	boolean accountLocked
+	boolean passwordExpired
+
+	static constraints = {
+		firstName(blank:false)
+        middleName(blank:true)
+        lastName(blank:false)
+         
+		username blank: false, unique: true
+		password blank: false
+	}
+
+	static mapping = {
+		password column: '`password`'
+	}
+
+	Set<Role> getAuthorities() {
+		UserRole.findAllByUser(this).collect { it.role } as Set
+	}
+
+	def beforeInsert() {
+		encodePassword()
+	}
+
+	def beforeUpdate() {
+		if (isDirty('password')) {
+			encodePassword()
+		}
+	}
+
+	protected void encodePassword() {
+		password = springSecurityService.encodePassword(password)
+	}
+	
+	
+	String toString(){
          "${firstName+" "+middleName+" "+lastName}";
     }
 }
