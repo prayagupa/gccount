99a3ec2a0417680b76f123f4ad8cd108f77e2601
iPrayag <prayag.upd@gmail.com>
2013-12-04 00:56:21 +0545

    REFACTOR grails.sh for running specific grails version mentioned in application.prop

--- grails.sh
+++ grails.sh
@@ -1,8 +1,15 @@
+#!/bin/bash
+
+GRAILS_DIRECTORY="/usr/local"
+GRAILS_VERSION=`grep app.grails.version application.properties | cut -d'=' -f2`
+GRAILS_HOME="$GRAILS_DIRECTORY/grails-$GRAILS_VERSION"
 GRAILS_OPTS="-Xmx2G -Xms1024m -XX:MaxPermSize=1024m"
-export GRAILS_OPTS
 
-grails test-app
+export GRAILS_HOME
+#export GRAILS_OPTS
+#$GRAILS_HOME/bin/grails $*
 
-echo "TESTS SUCCESSFUL..."
-
-grails -Dserver.port=8443 run-app
+$GRAILS_HOME/bin/grails compile
+#$GRAILS_HOME/bin/grails test-app
+#echo "TESTS SUCCESSFUL..."
+#$GRAILS_HOME/bin/grails -Dserver.port=8443 run-app
