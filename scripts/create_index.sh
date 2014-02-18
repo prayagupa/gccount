d52c6ea27303b2349009b0e5de8612a4768feb11
iPrayag <prayag.upd@gmail.com>
2014-02-19 00:51:00 +0545

    ADD index aliasing

--- scripts/create_index.sh
+++ scripts/create_index.sh
@@ -3,3 +3,13 @@ curl -XPUT 'http://localhost:9200/gccount/'
 echo
 echo "$indexName created."
 echo
+
+curl -XPOST 'http://localhost:9200/_aliases' -d '
+{
+    "actions" : [
+        { "add" : { "index" : "gccount", "alias" : "g" } }
+    ]
+}'
+
+echo "alias 'g' created"
+echo
