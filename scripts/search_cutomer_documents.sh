8413d605a61d64767b4e4c9c16e663611ab8bcf3
iPrayag <prayag.upd@gmail.com>
2014-02-26 17:43:18 +0545

    ADD nested query range filter example

--- scripts/search_cutomer_documents.sh
+++ scripts/search_cutomer_documents.sh
@@ -1,11 +1,11 @@
-<<MATCH_ALL
+<<MATCH_ALL_QUERY
 curl -X POST "http://localhost:9200/gccount/Customer/_search?pretty=true" -d '
   {
     "query" : { 
           "match_all": {} 
      }
   }'
-MATCH_ALL
+MATCH_ALL_QUERY
 
 curl -X POST "http://localhost:9200/gccount/Customer/_search?pretty=true" -d '
 {
@@ -58,3 +58,54 @@ curl -X POST "http://localhost:9200/gccount/Customer/_search?pretty=true" -d '
   }
 }
 FILTERED
+
+
+<<NESTED_FILTER_EX
+{
+  "fields": ["transactions"], 
+  "query": {
+    "filtered": {
+      "query": {
+        "match_all": {}
+      },
+      "filter": {
+        "nested": {
+          "path": "transactions",
+          "filter": {
+            "and": {
+              "filters": [
+                {
+                  "range": {
+                    "transactions.serviceDate": {
+                      "from": "2013-02-01",
+                      "to": "2014-01-31",
+                      "include_lower": true,
+                      "include_upper": true
+                    }
+                  }
+                },
+                {
+                  "range": {
+                    "transactions.paidDate": {
+                      "from": "2013-02-01",
+                      "to": "2014-01-31",
+                      "include_lower": true,
+                      "include_upper": true
+                    }
+                  }
+                },
+                {
+                  "term": {
+                    "transactions.type": "7"
+                  }
+                }
+              ]
+            }
+          }
+        }
+      }
+    }
+  }
+}
+NESTED_FILTER_EX
+
