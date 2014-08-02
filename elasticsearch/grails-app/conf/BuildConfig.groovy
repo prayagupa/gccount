d925b30112bddaa36fa3d49119779dedc4c681e7
Prayag Upd <prayag.upd@gmail.com>
2014-08-02 14:27:24 +0545

    REFACTOR analytics to make elasticsearch a separate plugin

--- elasticsearch/grails-app/conf/BuildConfig.groovy
+++ elasticsearch/grails-app/conf/BuildConfig.groovy
@@ -2,8 +2,6 @@ grails.project.class.dir = "target/classes"
 grails.project.test.class.dir = "target/test-classes"
 grails.project.test.reports.dir = "target/test-reports"
 
-grails.plugin.location.'elasticsearch' = "../elasticsearch"
-
 grails.project.fork = [
     // configure settings for compilation JVM, note that if you alter the Groovy version forked compilation is required
     //  compile: [maxMemory: 256, minMemory: 64, debug: false, maxPerm: 256, daemon:true],
